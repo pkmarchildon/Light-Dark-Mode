ec405861d2a64d6f3736ef13be490941d94ad87d
PK <59125527+pkmarchildon@users.noreply.github.com>
2021-11-01 09:24:56 -0400

    cleaned version

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const toggleText = document.querySelector('span[class="toggle-text"]');
-const toggleIcon = document.querySelector('span#toggle-icon i');
+const toggleLayoutMode = document.getElementById('toggle-icon');
 const aboutImage1 = document.getElementById('image1');
 const aboutImage2 = document.getElementById('image2');
 const aboutImage3 = document.getElementById('image3');
@@ -12,57 +11,47 @@ function switchTheme(event) {
    if (event.target.checked) {
       // Toggle is active, dark mode
       document.documentElement.setAttribute('data-theme', 'dark');
-      toggleSwitchDarkMode();
-      changePicturesDarkMode();
-      changeNavbarBackgroundDarkMode();
-      changeTextBoxBackgroundDarkMode();
+      localStorage.setItem('theme', 'dark');
+      toggleThemeMode(false);
    } else {
       // Toggle is not active, light mode
       document.documentElement.setAttribute('data-theme', 'light');
-      toggleSwitchLightMode();
-      changePicturesLightMode();
-      changeNavbarBackgroundLightMode();
-      changeTextBoxBackgroundLightMode();
+      localStorage.setItem('theme', 'light');
+      toggleThemeMode(true);
    }
 }
 
-function toggleSwitchDarkMode() {
-   toggleText.textContent = 'Light Mode';
-   toggleIcon.className = 'fas fa-sun';
+function toggleThemeMode(isLight) {
+   navBar.style.backgroundColor = isLight
+      ? 'rgb(255 255 255 / 50%)'
+      : 'rgb(0 0 0 / 50%)';
+   buttonsTextBox.style.backgroundColor = isLight
+      ? 'rgba(0, 0, 0, 0.5)'
+      : 'rgba(255, 255, 255, 0.5)';
+   toggleLayoutMode.children[0].textContent = isLight
+      ? 'Dark Mode'
+      : 'Light Mode';
+   toggleLayoutMode.children[1].className = isLight
+      ? 'fas fa-moon'
+      : 'fas fa-sun';
+   isLight ? changePictures('light') : changePictures('dark');
 }
 
-function toggleSwitchLightMode() {
-   toggleText.textContent = 'Dark Mode';
-   toggleIcon.className = 'fas fa-moon';
-}
-
-function changePicturesDarkMode() {
-   aboutImage1.setAttribute('src', '/img/undraw_proud_coder_dark.svg');
-   aboutImage2.setAttribute('src', '/img/undraw_feeling_proud_dark.svg');
-   aboutImage3.setAttribute('src', '/img/undraw_conceptual_idea_dark.svg');
-}
-
-function changePicturesLightMode() {
-   aboutImage1.setAttribute('src', '/img/undraw_proud_coder_light.svg');
-   aboutImage2.setAttribute('src', '/img/undraw_feeling_proud_light.svg');
-   aboutImage3.setAttribute('src', '/img/undraw_conceptual_idea_light.svg');
-}
-
-function changeNavbarBackgroundDarkMode() {
-   navBar.style.background = 'rgb(255 255 255 / 10%)';
-}
-
-function changeNavbarBackgroundLightMode() {
-   navBar.style.background = 'rgb(255 255 255 / 50%)';
-}
-
-function changeTextBoxBackgroundDarkMode() {
-   buttonsTextBox.style.background = 'rgba(255, 255, 255, 0.9)';
-}
-
-function changeTextBoxBackgroundLightMode() {
-   buttonsTextBox.style.background = 'rgb(0, 0, 0, 0.5)';
+function changePictures(mode) {
+   aboutImage1.src = `/img/undraw_proud_coder_${mode}.svg`;
+   aboutImage2.src = `/img/undraw_feeling_proud_${mode}.svg`;
+   aboutImage3.src = `/img/undraw_conceptual_idea_${mode}.svg`;
 }
 
 // Event listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local storage for theme. Always have to check if the key exists.
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+   document.documentElement.setAttribute('data-theme', currentTheme);
+   if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+      toggleThemeMode(false);
+   }
+}
